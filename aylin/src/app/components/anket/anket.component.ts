2e7f86d7746f9a04b59a30631a7b2185b4e59bdc
Mehmet Can Savaş <75901580+aylinaleyna@users.noreply.github.com>
2021-11-30 17:12:42 +0300

    vize

---
+++ aylin/src/app/components/anket/anket.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-anket',
+  templateUrl: './anket.component.html',
+  styleUrls: ['./anket.component.scss']
+})
+export class AnketComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
